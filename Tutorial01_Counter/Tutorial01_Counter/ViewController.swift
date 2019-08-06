144567daa86e8ed22e9aa5b76f363777dee9eb2a
Jing Yang <jingyang_carl@qq.com>
2019-08-06 14:46:09 -0700

    Finish Tutorial01_Counter

--- Tutorial01_Counter/Tutorial01_Counter/ViewController.swift
+++ Tutorial01_Counter/Tutorial01_Counter/ViewController.swift
@@ -2,7 +2,7 @@
 //  ViewController.swift
 //  Tutorial01_Counter
 //
-//  Created by qingguo xu on 8/6/19.
+//  Created by Jing Yang on 8/6/19.
 //  Copyright © 2019 Jing Yang. All rights reserved.
 //
 
@@ -10,11 +10,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var label: UILabel!
+    var number: Int = 0
+    
+    @IBAction func perform(_ sender: Any) {
+        
+        // the following code is the if-else statement in swift
+//        if let number = self.label.text {
+//            print(number)
+//        } else {
+//            print("value is null")
+//        }
+        
+        // the following code the same as if else statement
+//         guard let number = self.label.text else {return}
+//         print(number)
+        
+        self.number += 1
+        self.label.text = String(number)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
 }
 
