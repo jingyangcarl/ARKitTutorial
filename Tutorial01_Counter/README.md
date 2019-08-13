a7a844f47a3659ad7aa373be0cf6d88a046647e4
Jing Yang <jingyang_carl@qq.com>
2019-08-13 16:17:21 -0700

    Update readme for tutorial 01

---
+++ Tutorial01_Counter/README.md
@@ -0,0 +1,23 @@
+Tutorial 01 - Counter
+====================
+
+## Solution Introduction
+This tutorial introduces how to build an simple counter app using Xcode with swift. There is only one lable and one button, where a click on the button will trigger the label to increase by one.
+
+## Solution Environment:
+* MacOS Mojave 10.14.5
+* Xcode 10.3
+
+## Solution Documentation:
+> ViewController.swift
+>
+>> override func viewDidLoad(): This function is used to do initialization after the view is loaded;
+>>
+>> @IBAction func perform(_ sender: Any): This function is the callback function of the button, which is used to increase the label content by one;
+>
+
+## Solution Hierarchy:
+```
+./Tutorial01_Counter
+
+```
--- Tutorial01_Counter/Tutorial01_Counter/ViewController.swift
+++ Tutorial01_Counter/Tutorial01_Counter/ViewController.swift
@@ -13,26 +13,14 @@ class ViewController: UIViewController {
     @IBOutlet weak var label: UILabel!
     var number: Int = 0
     
-    @IBAction func perform(_ sender: Any) {
-        
-//        // the following code is the if-else statement in swift
-//        if let number = self.label.text {
-//            print(number)
-//        } else {
-//            print("value is null")
-//        }
-        
-//        // the following code the same as if else statement
-//         guard let number = self.label.text else {return}
-//         print(number)
-        
-        self.number += 1
-        self.label.text = String(number)
-    }
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func perform(_ sender: Any) {
+        self.number += 1
+        self.label.text = String(number)
     }
     
 }
